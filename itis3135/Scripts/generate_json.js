8bc037fdcbb3fd2cb4cc4eff8d804e4a1c7a8b0c
juansec04 <jramir62@charlotte.edu>
2026-03-23 23:17:50 -0400

    Update generate_json.js

--- itis3135/Scripts/generate_json.js
+++ itis3135/Scripts/generate_json.js
@@ -30,37 +30,37 @@ if (generateJsonBtn) {
     form.style.display = 'none';
 
     const jsonText = JSON.stringify({
-      first_name: data.firstName,
-      middle_name: data.middleName,
-      preferred_name: data.preferredName,
-      last_name: data.lastName,
-      acknowledgment_statement: data.ackStatement,
-      acknowledgment_date: data.ackDate,
-      mascot_adjective: data.mascotAdjective,
-      mascot_animal: data.mascotAnimal,
+      firstName: data.firstName,
+      middleName: data.middleName,
+      preferredName: data.preferredName,
+      lastName: data.lastName,
+      acknowledgmentStatement: data.ackStatement,
+      acknowledgmentDate: data.ackDate,
+      mascotAdjective: data.mascotAdjective,
+      mascotAnimal: data.mascotAnimal,
       divider: data.divider,
       image: data.picture,
-      image_caption: data.pictureCaption,
-      personal_statement: data.personalStatement,
-      personal_background: data.personalBackground,
-      professional_background: data.professionalBackground,
-      academic_background: data.academicBackground,
-      subject_background: data.subjectBackground,
-      primary_computer: data.primaryComputer,
-      funny_thing: data.funnyThing,
-      share_something: data.shareSomething,
+      imageCaption: data.pictureCaption,
+      personalStatement: data.personalStatement,
+      personalBackground: data.personalBackground,
+      professionalBackground: data.professionalBackground,
+      academicBackground: data.academicBackground,
+      subjectBackground: data.subjectBackground,
+      primaryComputer: data.primaryComputer,
+      funnyThing: data.funnyThing,
+      shareSomething: data.shareSomething,
       quote: data.quote,
-      quote_author: data.quoteAuthor,
+      quoteAuthor: data.quoteAuthor,
       courses: data.courses,
       links: data.links.map((href, i) => ({ name: `Link ${i + 1}`, href }))
-       }, null, 2);
+    }, null, 2);
 
     outputArea.innerHTML = `
-      <section>
-        <pre><code>${escapeHtml(jsonText)}</code></pre>
-        <p><button id="reset-json">Reset progress</button></p>
-      </section>
-    `;
+  <section>
+    <pre><code>${escapeHtml(jsonText)}</code></pre>
+    <p><button id="reset-json">Reset progress</button></p>
+  </section>
+`;
 
     document.getElementById('reset-json').addEventListener('click', () => window.location.reload());
   });
